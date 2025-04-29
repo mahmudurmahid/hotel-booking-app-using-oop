00a77a4135dab04f56d5fa8330d85b45daf37221
mahmudurmahid <m.mahid@icloud.com>
2025-04-29 17:18:24 +0100

    user can book room and change the availability of the hotel in the csv file

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pandas
 
-df = pandas.read_csv('hotels.csv')
+df = pandas.read_csv('hotels.csv', dtype={"id":str})
+
 
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book_room(self):
-        pass
+        """ Books a room in the hotel by changing the availability status to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv('hotels.csv', index=False)
 
     def available_room(self):
-        pass
+        """ Check if the hotel has available rooms """
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 
 class ReservationConfirmation:
@@ -22,8 +30,8 @@ class ReservationConfirmation:
 
 
 print(df)
-id = input("Enter the hotel ID: ")
-hotel = Hotel(id)
+hotel_ID = input("Enter the hotel ID: ")
+hotel = Hotel(hotel_ID)
 
 if hotel.available_room:
     hotel.book_room()
